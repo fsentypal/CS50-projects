fcf7d74352ad9a238cecfa9d246ee297558e7bb6
fsentypal <109785291+fsentypal@users.noreply.github.com>
2022-08-21 22:33:38 +0000

    Sun, Aug 21, 2022, 10:33 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -3,5 +3,22 @@ cd#include <cs50.h>
 
 int main(void)
 {
+ n = get_int("Size: ");
+    }
+    while (n < 1);
 
+    // For each row
+    for (int i = 0; i < n; i++)
+    {
+        // For each column
+        for (int j = 0; j < n; j++)
+        {
+            // Print a brick
+            printf("#");
+        }
+
+        // Move to next row
+        printf("\n");
+    }
+}
 }
