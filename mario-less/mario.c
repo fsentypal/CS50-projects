83533d641dc48fc9f4635fe03c5acfe5d818f8e6
fsentypal <109785291+fsentypal@users.noreply.github.com>
2022-08-22 06:18:36 +0000

    Mon, Aug 22, 2022, 6:18 AM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -10,15 +10,9 @@ int main(void)
         n = get_int("Height: ");
     } while (n < 1 || n > 8);
 
-    for(int i = 0; i < n; i++)
+    for(int j = 0; j < n; j++)
     {
-        for(int j = 0; j < n; j++)
-        {
-            if (i + j < n - 1)
-                printf(" ");
-            else
-                printf("#\n");
-        }
-        printf("\n");
+        for (int i = 0; i < n; i++)
+            printf("#\n");
     }
 }
