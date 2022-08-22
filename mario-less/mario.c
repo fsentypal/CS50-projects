06ed2a726d4387c0597627dfea631df4c20c2d49
fsentypal <109785291+fsentypal@users.noreply.github.com>
2022-08-22 06:02:52 +0000

    Mon, Aug 22, 2022, 6:02 AM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,7 +1,17 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    int n;
+    do
+    {
+        // get user input
+        n = get_int("Height: ");
+    } while(n < 1 || n > 8);
+
+    for(int i = 0; i < n; i++)
+    {
+        printf("#\n");
+    }
 }
