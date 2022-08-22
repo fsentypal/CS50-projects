4e8f56efa23aa6da8b2970c12558d455731c1352
fsentypal <109785291+fsentypal@users.noreply.github.com>
2022-08-22 06:28:04 +0000

    Mon, Aug 22, 2022, 6:28 AM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -12,8 +12,12 @@ int main(void)
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
-
-        printf("#");
+        {
+            if (i + j < n - 1)
+                printf(" ");
+            else
+                printf("#");
+        }
     printf("\n");
     }
 }
