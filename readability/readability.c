d3cf39d619571e8b52d253fab3cb9f422bb02a68
fsentypal <109785291+fsentypal@users.noreply.github.com>
2022-09-07 21:31:16 +0000

    Wed, Sep 7, 2022, 9:31 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -28,9 +28,9 @@ int main(void)
             sentences++;
         }
     }
-    float L = letters / words * 100
-    float S = sentences / words * 100
+    float L = (float) letters / (float) words * 100
+    float S = (float) sentences / (float) words * 100
 
     int index = round (0.0588 * L - 0.296 * S - 15.8);
-
+    
 }
