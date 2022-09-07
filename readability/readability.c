33bff72932d75f10d302d6f804de958e8c777dae
fsentypal <109785291+fsentypal@users.noreply.github.com>
2022-09-07 21:59:46 +0000

    Wed, Sep 7, 2022, 9:59 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -16,6 +16,7 @@ int main(void)
 
    for (int i = 0; i < strlen(text); i++)
    {
+        //Loop to check for letters, words, and sentences
        if (isalpha(text[i]))
        {
            letters++;
@@ -32,23 +33,28 @@ int main(void)
        }
    }
 
+    //
    float L = (float) letters / (float) words * 100;
    float S = (float) sentences / (float) words * 100;
 
+    //Use formula to calculate grade level
    int index = round(0.0588 * L - 0.296 * S - 15.8);
 
    if (index < 1)
    {
+        //If the index is less than the calculated Grade 1 print "Before Grade 1"
        printf("Before Grade 1\n");
    }
 
    else if (index > 16)
    {
+        //If the index is greater than the calculated Grade 16 print "Grade 16+"
        printf("Grade 16+\n");
    }
 
    else
    {
+        //If the index is in between 1 and 16 print "Grade %i"
        printf("Grade %i\n", index);
    }
 }
