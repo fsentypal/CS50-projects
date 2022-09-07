27dd7bc293088ce89030ca833035653c91a2b5e4
fsentypal <109785291+fsentypal@users.noreply.github.com>
2022-09-07 21:23:37 +0000

    Wed, Sep 7, 2022, 9:23 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -7,7 +7,7 @@ int main(void)
     string text = get_string("Text: ");
 
     int letters = 0;
-    int words = 0;
+    int words = 1;
     int sentences = 0;
 
     for (int i = 0; i < strlen(text); i++)
@@ -15,6 +15,11 @@ int main(void)
         if ((text[i] > 65 && text[i] < 90) || (text[i] > 97 && text[i] < 122))
         {
             letters++;
+        }
+
+        else if (text[i] == ' ')
+        {
+            words++;
         }
 
         
