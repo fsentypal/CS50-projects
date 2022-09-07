6e1d054c943f1e7bbbb6a3bf2c3be94c17592fe2
fsentypal <109785291+fsentypal@users.noreply.github.com>
2022-09-07 21:34:22 +0000

    Wed, Sep 7, 2022, 9:34 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -32,5 +32,19 @@ int main(void)
     float S = (float) sentences / (float) words * 100
 
     int index = round (0.0588 * L - 0.296 * S - 15.8);
-    
+
+    if (index < 1)
+    {
+        printf("Before Grade 1");
+    }
+
+    else if (index > 16)
+    {
+        printf("Grade 16+");
+    }
+
+    else
+    {
+        printf("Grade %i", index);
+    }
 }
