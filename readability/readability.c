23729d32d0fbe07eb275d122e2d8fecf0bc96f0a
fsentypal <109785291+fsentypal@users.noreply.github.com>
2022-09-07 21:16:01 +0000

    Wed, Sep 7, 2022, 9:16 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -4,4 +4,13 @@
 int main(void)
 {
     string text = get_string("Text: ");
+
+    int letters = 0;
+    int words = 0;
+    int sentences = 0;
+
+    for (int i = 0; i < strlen(text); i++)
+    {
+        if
+    }
 }
