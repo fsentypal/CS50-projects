f23a0643d6e2896137e7a93dcc555383e21aa0bf
Filip Sentypal <fsentypal13@gmail.com>
2023-08-22 18:26:03 +0000

    Tue, Aug 22, 2023, 11:26 AM -07:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,7 +1,56 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 
-int main(int argc, string argv[]) 
+bool only_digits(string s);
+char rotate(char c, int key);
+
+int main(int argc, string argv[])
 {
+    // Check for command-line arguments
+    if (argc != 2 || !only_digits(argv[1]))
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
 
+    // Convert key to integer
+    int key = atoi(argv[1]);
+
+    // Prompt user for plaintext
+    string plaintext = get_string("plaintext: ");
+    printf("ciphertext: ");
+
+    // Encrypt and print ciphertext
+    for (int i = 0, n = strlen(plaintext); i < n; i++)
+    {
+        printf("%c", rotate(plaintext[i], key));
+    }
+    printf("\n");
+
+    return 0;
+}
+
+bool only_digits(string s)
+{
+    for (int i = 0, n = strlen(s); i < n; i++)
+    {
+        if (!isdigit(s[i]))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+char rotate(char c, int key)
+{
+    if (isalpha(c))
+    {
+        char base = isupper(c) ? 'A' : 'a';
+        return (c - base + key) % 26 + base;
+    }
+    return c;
 }
