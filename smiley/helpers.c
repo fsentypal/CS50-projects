2b9aeb4cdcb975883f82851ac1b336b8655f138a
Filip Sentypal <fsentypal13@gmail.com>
2023-08-24 02:50:01 +0000

    Wed, Aug 23, 2023, 7:50 PM -07:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -2,17 +2,17 @@
 
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
-    // Iterate over each pixel in the image
+    // iterate over each pixel
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            // Check if the pixel is black
+            // check if the pixel is black
             if (image[i][j].rgbtRed == 0 && image[i][j].rgbtGreen == 0 && image[i][j].rgbtBlue == 0)
             {
                 // Change the pixel color to red
-                image[i][j].rgbtRed = 255;
-                image[i][j].rgbtGreen = 0;
+                image[i][j].rgbtRed = 0;
+                image[i][j].rgbtGreen = 255;
                 image[i][j].rgbtBlue = 0;
             }
         }
