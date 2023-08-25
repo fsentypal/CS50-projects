65c68ba2df928ad9c76c82ff5245248786398716
Filip Sentypal <fsentypal13@gmail.com>
2023-08-25 06:36:38 +0000

    Thu, Aug 24, 2023, 11:36 PM -07:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -2,6 +2,9 @@
 
 #include <ctype.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "dictionary.h"
 
@@ -18,6 +21,8 @@ const unsigned int N = 26;
 
 // Hash table
 node *table[N];
+
+unsigned int word_count = 0;
 
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
