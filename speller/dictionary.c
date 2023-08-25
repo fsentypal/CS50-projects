a40226400a8417def5c3fd94a323bae7b6139197
Filip Sentypal <fsentypal13@gmail.com>
2023-08-25 06:22:32 +0000

    Thu, Aug 24, 2023, 11:22 PM -07:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -19,10 +19,33 @@ const unsigned int N = 26;
 // Hash table
 node *table[N];
 
+// Variable to keep track of the number of words loaded into the dictionary
+unsigned int word_count = 0;
+
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
 {
-    // TODO
+    // Convert word to lowercase for case-insensitive comparison
+    char temp[strlen(word) + 1];
+    strcpy(temp, word);
+    for (int i = 0; temp[i]; i++)
+    {
+        temp[i] = tolower(temp[i]);
+    }
+
+    // Use the hash function to get the index of the bucket where the word might be located
+    int index = hash(temp);
+    node *cursor = table[index];
+
+    // Traverse the linked list in the bucket to find the word
+    while (cursor != NULL)
+    {
+        if (strcmp(cursor->word, temp) == 0)
+        {
+            return true;
+        }
+        cursor = cursor->next;
+    }
     return false;
 }
 
@@ -36,20 +59,50 @@ unsigned int hash(const char *word)
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
-    // TODO
-    return false;
+    FILE *file = fopen(dictionary, "r");
+    if (file == NULL)
+    {
+        return false;
+    }
+
+    char word[LENGTH + 1];
+    while (fscanf(file, "%s", word) != EOF)
+    {
+        node *new_node = malloc(sizeof(node));
+        if (new_node == NULL)
+        {
+            return false;
+        }
+        strcpy(new_node->word, word);
+        int index = hash(word);
+        new_node->next = table[index];
+        table[index] = new_node;
+        word_count++;
+    }
+
+    fclose(file);
+    return true;
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
 {
     // TODO
-    return 0;
+    return word_count;
 }
 
 // Unloads dictionary from memory, returning true if successful, else false
 bool unload(void)
 {
-    // TODO
-    return false;
+    for (int i = 0; i < N; i++)
+    {
+        node *cursor = table[i];
+        while (cursor != NULL)
+        {
+            node *temp = cursor;
+            cursor = cursor->next;
+            free(temp);
+        }
+    }
+    return true;
 }
