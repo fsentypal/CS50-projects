a9c04d7f704cbc3a317714f63d7ce3587a01af59
Filip Sentypal <fsentypal13@gmail.com>
2023-08-26 05:55:48 +0000

    Fri, Aug 25, 2023, 10:55 PM -07:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,1 +1,16 @@
-# TODO
+while True:
+    try:
+        change = float(input("Change owed: "))
+        if change >= 0:
+            break
+    except ValueError:
+        continue
+
+cents = round(change * 100)
+coins = 0
+
+for coin in [25, 10, 5, 1]:
+    coins += cents // coin
+    cents %= coin
+
+print(coins)
