3e0aeeb52b30d079baa06e4718702ab5330d1431
Filip Sentypal <fsentypal13@gmail.com>
2023-08-26 05:58:23 +0000

    Fri, Aug 25, 2023, 10:58 PM -07:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,16 +1,25 @@
 while True:
     try:
+        # ask for amount of change owed
         change = float(input("Change owed: "))
+
+        # if the input is non-negative, break
         if change >= 0:
             break
+    # if the input is not a number, continue
     except ValueError:
         continue
 
+# convert the change from dollars to cents
 cents = round(change * 100)
+# initialize a counter
 coins = 0
 
+# loop through each coin type
 for coin in [25, 10, 5, 1]:
+    # add the number of coins to the counter
     coins += cents // coin
+    # update the remaining cents
     cents %= coin
 
 print(coins)
