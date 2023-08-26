374b17bacf7f70b628134112d754bb21b74cc164
Filip Sentypal <fsentypal13@gmail.com>
2023-08-26 06:58:15 +0000

    Fri, Aug 25, 2023, 11:58 PM -07:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -11,20 +11,25 @@ AND EXISTS (
     AND ph.year = 2021 AND ph.month = 7 AND ph.day = 28 AND ph.duration < 60
 );
 
+
 SELECT a.city
 FROM airports a
-JOIN flights f ON a.id = f.destination_airport_id
-JOIN passengers pas ON f.id = pas.flight_id
-JOIN people p ON p.passport_number = pas.passport_number
-WHERE f.year = 2021 AND f.month = 7 AND f.day = 29 AND f.hour = 8 AND f.minute = 20
-AND p.name = "Bruce";  -- Replace "Bruce" with the name deduced from the previous query
+WHERE EXISTS (
+    SELECT 1 FROM flights f
+    JOIN passengers pas ON f.id = pas.flight_id
+    WHERE a.id = f.destination_airport_id
+    AND f.year = 2021 AND f.month = 7 AND f.day = 29 AND f.hour = 8 AND f.minute = 20
+    AND EXISTS (
+        SELECT 1 FROM people p
+        WHERE p.passport_number = pas.passport_number AND p.name = "Sofia"
+    )
+);
+
 
 SELECT p2.name
-FROM people p1
-JOIN passengers pas1 ON p1.passport_number = pas1.passport_number
-JOIN flights f1 ON pas1.flight_id = f1.id
-JOIN passengers pas2 ON f1.id = pas2.flight_id
-JOIN people p2 ON pas2.passport_number = p2.passport_number
-WHERE f1.year = 2021 AND f1.month = 7 AND f1.day = 29 AND f1.hour = 8 AND f1.minute = 20
-AND p1.name = "Bruce"  -- Replace "Bruce" with the name deduced from the first query
+FROM people p1, people p2, phone_calls ph
+WHERE p1.name = "Sofia"
+AND p1.phone_number = ph.caller
+AND p2.phone_number = ph.receiver
+AND ph.year = 2021 AND ph.month = 7 AND ph.day = 28 AND ph.duration < 60
 AND p1.name != p2.name;
