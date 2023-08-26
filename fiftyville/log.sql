936ee16bba388e803dbd7023b1e11ade214265da
Filip Sentypal <fsentypal13@gmail.com>
2023-08-26 06:49:14 +0000

    Fri, Aug 25, 2023, 11:49 PM -07:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -15,8 +15,6 @@ SuspiciousTransactions AS (
     SELECT account_number
     FROM atm_transactions
     WHERE year = 2021 AND month = 7 AND day = 28
-    AND atm_location = "Leggett Street"
-    AND transaction_type = "withdraw"
 ),
 SuspiciousFlights AS (
     SELECT passengers.passport_number
