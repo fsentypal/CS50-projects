225a85fbab26b53718169788c7208b73ace1ba18
Filip Sentypal <fsentypal13@gmail.com>
2023-08-26 05:53:08 +0000

    Fri, Aug 25, 2023, 10:53 PM -07:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,1 +1,15 @@
-# TODO
+# set height to 0
+height = 0
+
+# promt user until input is btwn 1 and 8
+while not 1 <= height <= 8:
+    try:
+        # prompt for height
+        height = int(input("Height: "))
+    except ValueError:
+        pass
+
+# loop through each lvl of the pyramid
+for i in range(1, height + 1):
+    # print spaces and hashes where needed
+    print(" " * (height - i) + "#" * i)
